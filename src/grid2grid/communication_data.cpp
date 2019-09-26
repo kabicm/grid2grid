1aac5685edd8bd24e6fe70cd9f2311013b6fe1d1
Marko Kabic <mkmarkokabic@gmail.com>
2019-09-27 00:05:42 +0200

    Trying out blaze for transpose.

--- src/grid2grid/communication_data.cpp
+++ src/grid2grid/communication_data.cpp
@@ -125,7 +125,7 @@ void copy_block_from_buffer(T *src_ptr, block<T> &b) {
 template <typename T>
 void communication_data<T>::copy_to_buffer() {
     // std::cout << "commuication data.copy_to_buffer()" << std::endl;
-#pragma omp parallel for schedule(dynamic, 1)
+// #pragma omp parallel for schedule(dynamic, 1)
     for (unsigned i = 0; i < mpi_messages.size(); ++i) {
         const auto &m = mpi_messages[i];
         block<T> b = m.get_block();
@@ -138,7 +138,7 @@ void communication_data<T>::copy_to_buffer() {
 template <typename T>
 void communication_data<T>::copy_to_buffer(int idx) {
     assert(idx >= 0 && idx+1 < package_ticks.size());
-#pragma omp parallel for schedule(dynamic, 1)
+// #pragma omp parallel for schedule(dynamic, 1)
     for (unsigned i = package_ticks[idx]; i < package_ticks[idx+1]; ++i) {
         const auto &m = mpi_messages[i];
         block<T> b = m.get_block();
